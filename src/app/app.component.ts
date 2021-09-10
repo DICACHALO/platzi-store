870a16fd09e0e147c57b2514fcecfb70f49b414a
Diana Chacon <dchacon871@unab.edu.co>
2021-09-10 15:51:54 -0500

    Organize routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,8 @@
-import { Component } from '@angular/core';
-import { Product } from "./product.model";
+import { Component } from "@angular/core";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'platzi-store';
-
-  power = 10;
-  
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'Es una camiseta unisex que se adapta a todo tipo de tamaños'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
-  
-  clickProduct(id: number){
-    console.log('product');
-    console.log(id);
-    
-  }
-
-}
+export class AppComponent {}
