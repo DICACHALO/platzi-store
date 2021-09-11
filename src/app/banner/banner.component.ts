ba7e8a2ac4f5d402d63a862887ddf4c521137b11
Diana Chacon <dchacon871@unab.edu.co>
2021-09-10 19:00:23 -0500

    Feat: Banner slides with swiper 4.5.1

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.scss"],
 })
 export class BannerComponent implements OnInit {
+  images: string[] = [
+    "assets/images/banner-1.jpg",
+    "assets/images/banner-2.jpg",
+    "assets/images/banner-3.jpg",
+  ];
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+  constructor() {}
+
+  ngOnInit() {}
 
 }
