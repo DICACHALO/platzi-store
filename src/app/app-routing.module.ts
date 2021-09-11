1310e1d3009d4959dd3d79bdbc0a1ca044eba9d1
Diana Chacon <dchacon871@unab.edu.co>
2021-09-10 21:00:50 -0500

    Feat: Page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ContactComponent } from "./contact/contact.component";
 import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { ProductsComponent } from "./products/products.component";
 
 
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: "contact",
     component: ContactComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
